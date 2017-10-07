cd560ba078d9ed793adc31718db0d9506ae14142
deeeed <abreton@siteed.net>
2017-10-07 19:23:20 +0800

    bump version 0.2.3

--- src/mobile-config.js
+++ src/mobile-config.js
@@ -10,7 +10,7 @@ App.info({
     author: 'Arthur Breton',
     email: 'abreton@siteed.net',
     website: 'https://www.passtis.pw',
-    version: '0.2.2'
+    version: '0.2.3'
 });
 
 // Set up resources such as icons and launch screens.
