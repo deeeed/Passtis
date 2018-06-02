e4e3af4520706edbaba81ceeb32c4158a331cc42
deeeed <abreton@siteed.net>
2018-06-02 14:16:01 +0800

    Add 1Password export method 'export1Password'

--- src/imports/api/server/methods.server.js
+++ src/imports/api/server/methods.server.js
@@ -1,10 +1,12 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import {logger as log} from "../../util/loggers";
-
+import {KeyAccounts} from "../model/KeyAccounts.model";
+import CryptoJS from 'crypto-js';
 import GeoIP from 'geoip-lite-country';
+const fs = require('fs');
 
 Meteor.methods({
-    ping: function() {
+    ping: function () {
         log.debug("ping / pong on the server");
         return "pong";
     },
@@ -27,9 +29,97 @@ Meteor.methods({
         georesult.ip = ip;
         return georesult;
     },
-    "user.config"({passphrase,pincode}={}) {
+    "user.config"({passphrase, pincode} = {}) {
         check(passphrase, String);
 
         //TODO
+    },
+    /**
+     * Export the list of passwords for the current user as csv file.
+     * CSV File format is compatible with 1Password.
+     *
+     * @param masterPassword
+     * @param serverFile
+     * @return {string}
+     */
+    "export1Password"({masterPassword, serverFile}={}) {
+        if (!this.userId) {
+            throw new Meteor.Error("Authentication required")
+        }
+        check(masterPassword, String)
+
+        const config = Meteor.user().config;
+        // Create enc/dec key from masterPassword
+        let enckey = CryptoJS.PBKDF2(masterPassword, config.salt, {keySize: 512 / 32, iterations: 10}).toString();
+
+
+        log.info(`Export for ${Meteor.userId()} - key=${enckey}`)
+        let res = "";
+        // List through password
+        KeyAccounts.find({}, {limit: 1000}).forEach((keyAccount) => {
+            log.info(`keyAccount processing ${keyAccount.name}`)
+            // Try to decrypt 3 times
+            // There was a bug that sometime made decryption fail, trying again when it occurs usually fixes it.
+            let success = true;
+            let sDecrypted;
+            try {
+                let decrypted = CryptoJS.AES.decrypt(keyAccount.secure, enckey);
+                sDecrypted = decrypted.toString(CryptoJS.enc.Utf8);
+            } catch (e1) {
+                log.error(`#1 Failed to process account ${keyAccount.name}`)
+                try {
+                    sDecrypted = decrypted.toString(CryptoJS.enc.Utf8);
+                } catch (e2) {
+                    log.error(`#2 Failed to process account ${keyAccount.name}`)
+                    try {
+                        sDecrypted = decrypted.toString(CryptoJS.enc.Utf8);
+                    } catch(e3) {
+                        log.error(`#3 Failed to process account ${keyAccount.name}`)
+                        log.error(`Secure data: ${keyAccount.secure}`)
+                        success = false;
+                    }
+                }
+            }
+            // log.info(`Success=${success} && sDecrypted=${sDecrypted}`)
+            if(success && sDecrypted) {
+                let decrypted = EJSON.parse(sDecrypted);
+                let csvLine = `"${nonEmpty(keyAccount.name)}","${nonEmpty(decrypted.host)}",${usersFormatter(decrypted.users)},"${nonEmpty(keyAccount.description)}",${extraUsersFormatter(decrypted.users)}\n`;
+                log.info(csvLine)
+                res += csvLine;
+            }
+        })
+        // Write csv file
+        fs.writeFile('1password.csv', res, 'utf8', function (err) {
+            if (err) {
+                console.log('Some error occured - file either not saved or corrupted file saved.');
+            } else{
+                console.log('It\'s saved!');
+            }
+        });
+        return res;
     }
 });
+
+const nonEmpty = (s) => {
+    return s ? s : "";
+}
+
+/** Utility functions to extra user infos **/
+const usersFormatter = (users) => {
+    if(users && users.length>0) {
+        return `"${nonEmpty(users[0].login)}","${nonEmpty(users[0].password)}"`;
+    } else
+        return `""`;
+}
+const extraUsersFormatter = (users) => {
+    if(users && users.length<2)
+        return `""`;
+
+    let len=users.length;
+    let res = ``;
+
+    for(let i=1; i<len; i++) {
+        res += `"${nonEmpty(users[i].login)} / ${nonEmpty(users[i].password)}",`;
+    }
+    return res;
+}
